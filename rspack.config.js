10dbe1ee2cf49e5209c34abe4ba220338566bdf6
a0716116z <taylor.wu@iss.nthu.edu.tw>
2024-09-05 17:18:21 +0800

    wip: enable the rspack

---
+++ rspack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const defineConfig =require("@rspack/cli");
+const rspack =require("@rspack/core");
+const RefreshPlugin= require("@rspack/plugin-react-refresh") ;
+const isDev = process.env.NODE_ENV === "development";
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
+
+module.exports ={
+	context: __dirname,
+	entry: {
+		main: path.join( __dirname,"./src/index.jsx")
+	},
+	resolve: {
+		extensions: ["...", ".ts", ".tsx", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: "asset"
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: true
+								},
+								transform: {
+									react: {
+										runtime: "automatic",
+										development: isDev,
+										refresh: isDev
+									}
+								}
+							},
+							env: { targets }
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new rspack.HtmlRspackPlugin({
+			template: "./src/index.html"
+		}),
+		isDev ? new RefreshPlugin() : null
+	].filter(Boolean),
+	optimization: {
+		minimizer: [
+			new rspack.SwcJsMinimizerRspackPlugin(),
+			new rspack.LightningCssMinimizerRspackPlugin({
+				minimizerOptions: { targets }
+			})
+		]
+	},
+	experiments: {
+		css: true
+	}
+};
